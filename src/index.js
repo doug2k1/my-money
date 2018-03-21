680f42bbdbaa7e4a11406c3d18c85c4cc7d83cf9
Douglas Matoso <doug2k1@gmail.com>
2018-03-21 19:43:09 -0300

    Add Forest Admin.

--- src/index.js
+++ src/index.js
@@ -1,7 +1,20 @@
 const path = require('path');
 const express = require('express');
+const ForestAdmin = require('forest-express-sequelize');
+const { sequelize } = require('./models');
+const secret = require('../config/secret');
 
 const app = express();
+
+// Forest Admin
+app.use(
+  ForestAdmin.init({
+    modelsDir: path.resolve('./src/models'),
+    envSecret: secret.FOREST_ENV_SECRET,
+    authSecret: secret.FOREST_AUTH_SECRET,
+    sequelize
+  })
+);
 
 // static files
 app.use(express.static('public'));
